cdd2cba6bce294f2e372fb257c25dad999b39da5
yannbcf <Yannbeats.pro@gmail.com>
2024-08-11 15:22:38 +0200

    feat: update runtime entry point

--- Client/source/main.cpp
+++ Client/source/main.cpp
@@ -1,6 +1,7 @@
 #include "Runtime.h"
 
 #include <v-sdk/factories/RuntimeFactory.hpp>
+#include <spdlog/spdlog.h>
 #include <fw/Logger.h>
 #include <Windows.h>
 
@@ -8,27 +9,34 @@ namespace sdk = yamp::sdk;
 
 BOOL WINAPI DllMain(HINSTANCE instanceDll, DWORD reason, LPVOID reserved)
 {
-    fw::Logger* logger = fw::Logger::Get("js");
+    spdlog::set_pattern(fmt::format("[{}] [{}] {}", fw::Logger::TimePattern, fw::Logger::LogPattern, fw::Logger::MessagePattern));
+
+    fw::Logger* logger = fw::Logger::Get("JS");
     logger->SetPattern(fmt::format("[JS] {}", fw::Logger::MessagePattern));
 
     if (reason == DLL_PROCESS_ATTACH)
     {
-        auto runtimeFactory = sdk::IRuntimeFactory::GetInstance();
-
         logger->Info("Runtime has been loaded! {}", fmt::ptr(reserved));
         logger->Info("Author: yamp");
         logger->Info("Version: 0.0.1-alpha");
+
+        auto runtimeFactory = sdk::IRuntimeFactory::GetInstance();
         logger->Info("Runtime factory instance: {}\n", fmt::ptr(runtimeFactory));
 
         js::Runtime* runtime = js::Runtime::GetInstance();
-        sdk::Result result = runtimeFactory->RegisterRuntime(runtime);
 
+        // TODO: terminate the process if sdk::Return is a falsy value
+        sdk::Result result = runtimeFactory->RegisterRuntime(runtime);
         result = runtimeFactory->RegisterRuntimeResourceHandling("js", runtime);
+
+        // TODO: add a state to the runtime to know if v8 was succesfuly initialized
+        // TODO: should be called from the sdk
         runtime->OnStart();
     }
 
     if (reason == DLL_PROCESS_DETACH)
     {
+        // TODO: handle dll unloading
         logger->Info("Runtime is unloading...");
     }
 
