1a5886477e7963b370f245159b4d0c422bf10036
ImperfectFox <196264061+ImperfectFox@users.noreply.github.com>
2025-06-09 13:13:23 +0200

    chore: remove project files

--- Client/source/helpers/misc.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace helpers
-{
-    void Assert(bool condition, std::string_view error, bool terminate = true);
-    std::string ToCamelCase(std::string_view string);
-} // namespace helpers
--- Client/source/io/files.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace io
-{
-    std::string ReadFilePipe(FILE* pipe);
-    std::optional<std::string> ReadFile(std::string_view path, bool isTypescript = false);
-} // namespace io
