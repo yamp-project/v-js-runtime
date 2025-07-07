45fbfe6a8d2057ce7af76e68dfe5c10165829775
ByAlexius <alexander@rziha.at>
2025-07-07 22:21:33 +0200

    feat: v8 cmake script

--- vendors/v8.cmake
+++ vendors/v8.cmake
@@ -12,14 +12,14 @@ set(RUNTIME_DEPOT_TOOLS_REPO "https://chromium.googlesource.com/chromium/tools/d
 set(RUNTIME_DEPOT_TOOLS_TAG "main")
 
 function(depot_tools_update)
-    if (UNIX)
+    if (WIN32)
         execute_process(
-                COMMAND "./update_depot_tools"
+                COMMAND "./update_depot_tools.bat"
                 WORKING_DIRECTORY ${RUNTIME_DEPOT_TOOLS_REPO}
         )
     else ()
         execute_process(
-                COMMAND "./update_depot_tools.bat"
+                COMMAND "./update_depot_tools"
                 WORKING_DIRECTORY ${RUNTIME_DEPOT_TOOLS_REPO}
         )
     endif ()
@@ -99,10 +99,114 @@ else ()
     message(STATUS "Updating v8")
 
     execute_process(
-            COMMAND "git" "checkout" "origin/${RUNTIME_V8_TAG}"
-            COMMAND "git" "pull"
+            COMMAND "git" "checkout" "-B" "origin/${RUNTIME_V8_TAG}"
+            COMMAND "git" "pull" "origin/${RUNTIME_V8_TAG}"
             COMMAND "${RUNTIME_DEPOT_TOOLS_LOCATION}/gclient" "sync"
             WORKING_DIRECTORY ${RUNTIME_V8_LOCATION}
     )
 endif ()
 
+# V8 build process
+option(BUILD_V8 "Build V8 Javascript engine" OFF)
+
+if (${BUILD_V8})
+    message(STATUS "Building V8")
+
+    if (WIN32)
+        set(GN_EXECUTABLE "${RUNTIME_DEPOT_TOOLS_LOCATION}/gn.bat")
+    else ()
+        set(GN_EXECUTABLE "${RUNTIME_DEPOT_TOOLS_LOCATION}/gn")
+    endif ()
+
+    set(TREAT_WARNINGS_AS_ERRORS "false")
+    set(IS_OFFICIAL_BUILD "false")
+    set(IS_COMPONENT_BUILD "false")
+    set(IS_DEBUG "false")
+    set(SYMBOL_LEVEL "0")
+    set(STRIP_DEBUG_INFO "true")
+    set(USE_CUSTOM_LIBCXX "false")
+    set(V8_MONOLITHIC "true")
+    set(V8_ENABLE_SANDBOX "true")
+    set(V8_ENABLE_POINTER_COMPRESSION "true")
+    set(V8_ENABLE_WEBASSEMBLY "true")
+    set(V8_ENABLE_GDBJIT "false")
+    set(V8_ENABLE_I18N_SUPPORT "true")
+    set(V8_ENABLE_TEST_FEATURES "false")
+    set(V8_USE_EXTERNAL_STARTUP_DATA "false")
+
+    if(WIN32)
+        set(IS_CLANG "false")
+    else ()
+        set(USE_SYSROOT "false")
+        set(IS_CLANG "true")
+    endif()
+
+    set(V8_BUILD_DIR "out.gn")
+
+    set(GN_ARGS
+            "treat_warnings_as_errors=${TREAT_WARNINGS_AS_ERRORS}"
+            "is_official_build=${IS_OFFICIAL_BUILD}"
+            "is_component_build=${IS_COMPONENT_BUILD}"
+            "is_debug=${IS_DEBUG}"
+            "symbol_level=${SYMBOL_LEVEL}"
+            "strip_debug_info=${STRIP_DEBUG_INFO}"
+            "use_custom_libcxx=${USE_CUSTOM_LIBCXX}"
+            "v8_monolithic=${V8_MONOLITHIC}"
+            "v8_enable_sandbox=${V8_ENABLE_SANDBOX}"
+            "v8_enable_pointer_compression=${V8_ENABLE_POINTER_COMPRESSION}"
+            "v8_enable_webassembly=${V8_ENABLE_WEBASSEMBLY}"
+            "v8_enable_gdbjit=${V8_ENABLE_GDBJIT}"
+            "v8_enable_i18n_support=${V8_ENABLE_I18N_SUPPORT}"
+            "v8_enable_test_features=${V8_ENABLE_TEST_FEATURES}"
+            "v8_use_external_startup_data=${V8_USE_EXTERNAL_STARTUP_DATA}"
+    )
+
+    if(WIN32)
+        list(APPEND GN_ARGS "is_clang=${IS_CLANG}")
+    else ()
+        list(APPEND GN_ARGS "use_sysroot=${USE_SYSROOT}")
+        list(APPEND GN_ARGS "is_clang=${IS_CLANG}")
+    endif()
+
+    string(REPLACE ";" " " GN_ARGS_STRING "${GN_ARGS}")
+
+    find_program(NINJA_EXECUTABLE ninja REQUIRED)
+
+    add_custom_target(gn_generate
+        COMMAND ${CMAKE_COMMAND} -E echo "Running gn build..."
+        COMMAND ${GN_EXECUTABLE} gen ${V8_BUILD_DIR} --args=${GN_ARGS_STRING}
+        WORKING_DIRECTORY ${RUNTIME_V8_LOCATION}
+        COMMENT "Generating V8 build files with GN"
+    )
+
+    add_custom_target(v8_build
+            COMMAND ${NINJA_EXECUTABLE} -C ${V8_BUILD_DIR} v8_monolith
+            WORKING_DIRECTORY ${RUNTIME_V8_LOCATION}
+            DEPENDS gn_generate
+            COMMENT "Building V8 with Ninja"
+    )
+
+    add_custom_target(build_v8
+            COMMAND ${CMAKE_COMMAND} -E echo "Finished building V8 from source."
+            DEPENDS v8_build
+            COMMENT "Complete V8 build process"
+    )
+
+    add_library(V8::V8 SHARED IMPORTED GLOBAL)
+    if(WIN32)
+        set(V8_LIBRARY_NAME "v8_monolith.lib")
+        set(V8_LIBRARY_PATH "${RUNTIME_V8_LOCATION}/${V8_BUILD_DIR}/obj/v8_monolith.lib")
+    else()
+        set(V8_LIBRARY_NAME "libv8_monolith.a")
+        set(V8_LIBRARY_PATH "${RUNTIME_V8_LOCATION}/${V8_BUILD_DIR}/obj/libv8_monolith.a")
+    endif()
+
+    set_target_properties(V8::V8 PROPERTIES
+            IMPORTED_LOCATION "${V8_LIBRARY_PATH}"
+            INTERFACE_INCLUDE_DIRECTORIES "${RUNTIME_V8_LOCATION}/include"
+    )
+
+    add_dependencies(V8::V8 build_v8)
+else ()
+    message(STATUS "V8 build disabled. Use -DBUILD_V8=ON to enable.")
+endif ()
