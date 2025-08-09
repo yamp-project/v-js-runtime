de19c80606d5cc2b72a9f0f8f86e467cb94ceb57
ByAlexius <personal.git@my-server.at>
2025-08-09 22:33:59 +0200

    feat: util for v8 String and file reading

--- shared/src/util/utils.h
+++ shared/src/util/utils.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <cctype>
+#include <fstream>
 
 #define MAX_STR_SIZE = 64;
 
@@ -39,6 +40,21 @@ namespace utils
         }
 
         return result;
+    }
+
+    inline std::string ReadFile(const std::string& path) {
+        std::ifstream file(path.c_str());
+        if (!file.is_open()) {
+            return "";
+        }
+
+        std::string content((std::istreambuf_iterator(file)), std::istreambuf_iterator<char>());
+
+        return content;
+    }
+
+    inline v8::Local<v8::String> StringToV8(v8::Isolate* isolate, const std::string& str) {
+        return v8::String::NewFromUtf8(isolate, str.c_str()).ToLocalChecked();
     }
 }
 
