6e597f73861a7442cda873aa8a356f8be1856284
ByAlexius <personal.git@my-server.at>
2025-08-09 22:35:41 +0200

    feat: (wip) js source compiling and callback resolving

--- client/src/core/resource.cpp
+++ client/src/core/resource.cpp
@@ -2,8 +2,9 @@
 
 #include <filesystem>
 
-namespace js
-{
+#include "util/utils.h"
+
+namespace js {
     Resource::Resource(ILookupTable* lookupTable, IResource* resource, v8::Isolate* isolate) : m_Resource(resource), m_Logger(Logger(lookupTable, std::format("resource {}", resource->name))), m_Isolate(isolate)
     {
     }
@@ -14,6 +15,26 @@ namespace js
         std::filesystem::path mainFilePath = m_Resource->mainFile;
 
         v8::Isolate::Scope scope(m_Isolate);
+        v8::HandleScope handleScope(m_Isolate);
+        v8::Local<v8::Context> context = v8::Context::New(m_Isolate);
+
+        context->SetAlignedPointerInEmbedderData(1, this);
+
+        v8::Context::Scope contextScope(context);
+
+        v8::Local<v8::String> jsSource = utils::StringToV8(m_Isolate, utils::ReadFile(m_Resource->mainFile));
+
+        v8::ScriptCompiler::Source scriptSource(jsSource);
+        v8::Local<v8::Module> scriptModule;
+        if (!v8::ScriptCompiler::CompileModule(m_Isolate, &scriptSource).ToLocal(&scriptModule)) {
+            m_Logger.Warn("Failed to compile script");
+            this->OnStop();
+            return;
+        }
+
+        v8::Maybe<bool> result = scriptModule->InstantiateModule(context, Resource::ResolveCallback, nullptr);
+
+        //
 
         m_Logger.Info("Running resource %s with main file %s and resource path %s!", m_Resource->name, mainFilePath.c_str(), resourcePath.c_str());
     }
@@ -34,5 +55,13 @@ namespace js
 
     void Resource::OnEvent(const char* name, CAnyArray* args)
     {
+    }
+
+    v8::MaybeLocal<v8::Module> ResolveCallback(v8::Local<v8::Context> context,
+        v8::Local<v8::String> specifier, v8::Local<v8::FixedArray> import_attributes, v8::Local<v8::Module> referrer) {
+
+        auto* selfRef = static_cast<Resource*>(context->GetAlignedPointerFromEmbedderData(1));
+
+        std::string resourcePath = selfRef->GetResourcePath();
     }
 } // js
--- client/src/core/resource.h
+++ client/src/core/resource.h
@@ -1,6 +1,8 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
+#include <map>
+
 #include "util/logger.h"
 
 #include <yamp-sdk/sdk.h>
@@ -17,7 +19,19 @@ namespace js {
 
         Resource(ILookupTable* lookupTable, IResource* resource, v8::Isolate* isolate);
         ~Resource() = default;
+
+        std::string GetResourcePath() {
+            return m_Resource->path;
+        }
+
     private:
+        static v8::MaybeLocal<v8::Module> ResolveCallback(v8::Local<v8::Context> context,
+                                                v8::Local<v8::String> specifier,
+                                                v8::Local<v8::FixedArray> import_attributes,
+                                                v8::Local<v8::Module> referrer);
+
+        std::map<std::string, v8::Global<v8::Module>> m_ProcessedModules;
+
         IResource* m_Resource;
         Logger m_Logger;
 
