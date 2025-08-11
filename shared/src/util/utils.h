8b80d2dab62a816ca42976c29540221a886ca003
ByAlexius <personal.git@my-server.at>
2025-08-11 23:08:51 +0200

    chore: remove unused utils method

--- shared/src/util/utils.h
+++ shared/src/util/utils.h
@@ -9,39 +9,6 @@
 
 namespace utils
 {
-    inline std::string StrToCamelCase(const char* input)
-    {
-        if (input == nullptr) {
-            return {};
-        }
-
-        bool to_upper = false;
-        std::string result;
-
-        for (size_t i = 0; input[i] != '\0'; ++i) {
-            if (i >= MAX_STR_SIZE) {
-                break;
-            }
-
-            char ch = input[i];
-            if (ch == '_') {
-                to_upper = true;
-                continue;
-            }
-
-            if (result.empty()) {
-                result += std::tolower(static_cast<unsigned char>(ch));
-            } else if (to_upper) {
-                result += std::toupper(static_cast<unsigned char>(ch));
-                to_upper = false;
-            } else {
-                result += std::tolower(static_cast<unsigned char>(ch));
-            }
-        }
-
-        return result;
-    }
-
     inline std::string ReadFile(const std::string& path) {
         std::ifstream file(path.c_str());
         if (!file.is_open()) {
