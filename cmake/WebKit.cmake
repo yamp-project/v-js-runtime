4bcb80bacae0f2b56947cae618ed8f832685c903
ByAlexius <personal.git@my-server.at>
2025-08-18 21:07:16 +0200

    feat: fix WebKit.cmake

--- cmake/WebKit.cmake
+++ cmake/WebKit.cmake
@@ -11,46 +11,94 @@ function(setup_webkit_jsc)
         find_program(PYTHON_EXECUTABLE python3 python REQUIRED)
     endif()
 
-    message(STATUS "Found Perl: ${PERL_EXECUTABLE}")
-    message(STATUS "Found Python: ${PYTHON_EXECUTABLE}")
-    message(STATUS "Found Ruby: ${RUBY_EXECUTABLE}")
-    message(STATUS "Found Ninja: ${NINJA_EXECUTABLE}")
+    if(RUBY_EXECUTABLE)
+        execute_process(
+                COMMAND ${RUBY_EXECUTABLE} -e "puts RUBY_VERSION"
+                OUTPUT_VARIABLE RUBY_VERSION_OUTPUT
+                OUTPUT_STRIP_TRAILING_WHITESPACE
+                ERROR_QUIET
+                RESULT_VARIABLE RUBY_VERSION_RESULT
+        )
 
-    if(UNIX AND NOT APPLE)
-        find_package(PkgConfig REQUIRED)
-        pkg_check_modules(ICU REQUIRED icu-uc icu-i18n)
-        pkg_check_modules(GLIB2 REQUIRED glib-2.0)
-        message(STATUS "Found ICU: ${ICU_VERSION}")
-        message(STATUS "Found GLib: ${GLIB2_VERSION}")
+        if(RUBY_VERSION_RESULT EQUAL 0 AND RUBY_VERSION_OUTPUT)
+            string(REPLACE "." ";" RUBY_VERSION_LIST ${RUBY_VERSION_OUTPUT})
+            list(GET RUBY_VERSION_LIST 0 RUBY_MAJOR)
+            list(GET RUBY_VERSION_LIST 1 RUBY_MINOR)
+
+            if(RUBY_MAJOR LESS 2 OR (RUBY_MAJOR EQUAL 2 AND RUBY_MINOR LESS 5))
+                message(FATAL_ERROR "Ruby 2.5 or higher is required. Found Ruby ${RUBY_VERSION_OUTPUT}")
+            endif()
+
+            message(STATUS "Found Ruby: ${RUBY_EXECUTABLE} (version ${RUBY_VERSION_OUTPUT})")
+        else()
+            message(FATAL_ERROR "Could not determine Ruby version. Please ensure Ruby 2.5+ is installed.")
+        endif()
     endif()
 
+    message(STATUS "Found Perl: ${PERL_EXECUTABLE}")
+    message(STATUS "Found Python: ${PYTHON_EXECUTABLE}")
+    message(STATUS "Found Ninja: ${NINJA_EXECUTABLE}")
+
+    # Set up directories
+    set(WEBKIT_SOURCE_DIR "${CMAKE_SOURCE_DIR}/vendors/webkit")
+    set(WEBKIT_BUILD_DIR "${CMAKE_BINARY_DIR}/webkit-build")
+    set(WEBKIT_INSTALL_DIR "${CMAKE_BINARY_DIR}/webkit-install")
+
     if(WIN32)
-        set(WEBKIT_PORT "WinCairo")
+        set(WEBKIT_PORT "Win")
         set(JSC_LIBRARY_NAME "JavaScriptCore.lib")
         set(JSC_LIBRARY_SUBDIR "lib")
-        set(WEBKIT_CMAKE_ARGS -DUSE_WINCAIRO=ON -DCMAKE_SYSTEM_VERSION=10.0)
+
+        set(WEBKIT_CLANG_ARGS)
+        if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+            execute_process(
+                    COMMAND ${CMAKE_CXX_COMPILER} --print-resource-dir
+                    OUTPUT_VARIABLE CLANG_RESOURCE_DIR
+                    OUTPUT_STRIP_TRAILING_WHITESPACE
+                    ERROR_QUIET
+            )
+
+            if(CLANG_RESOURCE_DIR)
+                find_library(CLANG_BUILTINS_LIBRARY
+                        NAMES clang_rt.builtins-x86_64 clang_rt.builtins_x86_64 clang_rt.builtins
+                        PATHS
+                        "${CLANG_RESOURCE_DIR}/lib/windows"
+                        "${CLANG_RESOURCE_DIR}/lib"
+                        "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/lib/clang/19/lib/windows"
+                        "C:/Program Files/Microsoft Visual Studio/2022/Professional/VC/Tools/Llvm/x64/lib/clang/19/lib/windows"
+                        "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Tools/Llvm/x64/lib/clang/19/lib/windows"
+                        NO_DEFAULT_PATH
+                )
+
+                if(CLANG_BUILTINS_LIBRARY)
+                    message(STATUS "Found Clang builtins: ${CLANG_BUILTINS_LIBRARY}")
+                    list(APPEND WEBKIT_CLANG_ARGS -DCLANG_BUILTINS_LIBRARY=${CLANG_BUILTINS_LIBRARY})
+                else()
+                    list(APPEND WEBKIT_CLANG_ARGS -DUSE_CLANG_BUILTINS=OFF)
+                endif()
+            else()
+                list(APPEND WEBKIT_CLANG_ARGS -DUSE_CLANG_BUILTINS=OFF)
+            endif()
+        endif()
     elseif(APPLE)
         set(WEBKIT_PORT "Mac")
         set(JSC_LIBRARY_NAME "libJavaScriptCore.a")
         set(JSC_LIBRARY_SUBDIR "lib")
-        set(WEBKIT_CMAKE_ARGS -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15)
+        set(WEBKIT_CLANG_ARGS)
     else()
         set(WEBKIT_PORT "GTK")
         set(JSC_LIBRARY_NAME "libJavaScriptCore.a")
         set(JSC_LIBRARY_SUBDIR "lib")
-        set(WEBKIT_CMAKE_ARGS -DUSE_GTK4=OFF -DUSE_LIBSECRET=OFF)
+        set(WEBKIT_CLANG_ARGS)
     endif()
 
-    set(WEBKIT_INSTALL_DIR "${CMAKE_BINARY_DIR}/webkit-install")
-    set(WEBKIT_BUILD_DIR "${CMAKE_BINARY_DIR}/webkit-build")
-    set(WEBKIT_SOURCE_DIR "${CMAKE_SOURCE_DIR}/vendors/webkit")
     set(JSC_LIBRARY_PATH "${WEBKIT_INSTALL_DIR}/${JSC_LIBRARY_SUBDIR}/${JSC_LIBRARY_NAME}")
 
     if(NOT EXISTS "${WEBKIT_SOURCE_DIR}/Source/JavaScriptCore/CMakeLists.txt")
-        message(FATAL_ERROR "WebKit source not found. Please run: git submodule update --init --recursive")
+        message(FATAL_ERROR "Patched WebKit source not found at ${WEBKIT_SOURCE_DIR}. Please ensure your WebKit fork is properly set up as a submodule.")
     endif()
 
-    set (JSC_CLEAN_INCLUDE_DIR "${CMAKE_BINARY_DIR}/jsc-include")
+    set(JSC_CLEAN_INCLUDE_DIR "${CMAKE_BINARY_DIR}/jsc-include")
     file(MAKE_DIRECTORY "${JSC_CLEAN_INCLUDE_DIR}/JavaScriptCore")
 
     file(COPY "${WEBKIT_SOURCE_DIR}/Source/JavaScriptCore/API/"
@@ -59,25 +107,92 @@ function(setup_webkit_jsc)
 
     set(JSC_INCLUDE_DIR "${JSC_CLEAN_INCLUDE_DIR}")
 
+    include(${CMAKE_SOURCE_DIR}/cmake/ICU.cmake)
+
     ExternalProject_Add(WebKit_External
             SOURCE_DIR ${WEBKIT_SOURCE_DIR}
             BINARY_DIR ${WEBKIT_BUILD_DIR}
             INSTALL_DIR ${WEBKIT_INSTALL_DIR}
+            DEPENDS ICU_External
             CMAKE_ARGS
             -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
             -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
             -DCMAKE_GENERATOR=Ninja
+            -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+            -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+            -DCMAKE_MAKE_PROGRAM=${NINJA_EXECUTABLE}
             -DPORT=${WEBKIT_PORT}
+            -DRUBY_EXECUTABLE=${RUBY_EXECUTABLE}
+            -DPERL_EXECUTABLE=${PERL_EXECUTABLE}
+            -DPYTHON_EXECUTABLE=${PYTHON_EXECUTABLE}
+            -DICU_ROOT=${ICU_ROOT}
+
+            # Core JSC-only configuration
             -DENABLE_STATIC_JSC=ON
+            -DENABLE_XSLT=OFF
+            -DUSE_SKIA=OFF
+            -DUSE_CAIRO=OFF
+            -DENABLE_INTL=ON
+            -DENABLE_WEBGL=OFF
+            -DUSE_CAPSTONE=OFF
+            -DUSE_ANGLE_EGL=OFF
+            -DUSE_SYSTEM_UNIFDEF=OFF
             -DENABLE_WEBKIT=OFF
             -DENABLE_WEBCORE=OFF
+            -DENABLE_WEBKIT_LEGACY=OFF
+            -DENABLE_LAYOUT_TESTS=OFF
             -DENABLE_TOOLS=OFF
             -DENABLE_MINIBROWSER=OFF
             -DENABLE_WEBDRIVER=OFF
+            -DENABLE_WEBGPU=OFF
             -DENABLE_API_TESTS=OFF
+            -DUSE_XDGMIME=OFF
+            -DUSE_LIBWEBRTC=OFF
+            -DUSE_SYSTEM_SYSPROF_CAPTURE=OFF
+            -DUSE_SYSPROF_CAPTURE=OFF
+            -DENABLE_WEBINSPECTORUI=OFF
+
+            # Disable graphics and imaging
+            -DENABLE_GRAPHICS=OFF
+            -DUSE_ANGLE=OFF
+            -DUSE_ZLIB=OFF
+            -DUSE_AVIF=OFF
+            -DUSE_JPEGXL=OFF
+            -DUSE_LCMS=OFF
+            -DUSE_WOFF2=OFF
+            -DENABLE_IMAGE_FORMATS=OFF
+
+            # Disable networking
+            -DENABLE_NETWORKING=OFF
+            -DENABLE_TEXT_RENDERING=OFF
+            -DENABLE_XML_SUPPORT=OFF
+            -DENABLE_DATABASE=OFF
+            -DENABLE_THEMING=OFF
+
+            # Disable features that require external dependencies or are browser-only
             -DENABLE_VIDEO=OFF
             -DENABLE_WEB_AUDIO=OFF
-            ${WEBKIT_CMAKE_ARGS}
+            -DENABLE_SAMPLING_PROFILER=OFF
+            -DENABLE_WEBASSEMBLY=OFF
+            -DENABLE_WEB_RTC=OFF
+            -DENABLE_MEDIA_STREAM=OFF
+            -DENABLE_GAMEPAD=OFF
+            -DENABLE_GEOLOCATION=OFF
+            -DENABLE_ENCRYPTED_MEDIA=OFF
+
+            # JIT configuration (enable what's supported/stable)
+            -DENABLE_JIT=ON
+            -DENABLE_YARR_JIT=ON
+            -DENABLE_CONCURRENT_JIT=OFF
+            -DENABLE_DFG_JIT=ON
+            -DENABLE_FTL_JIT=OFF
+
+            # Keep minimal debugging support
+            -DENABLE_REMOTE_INSPECTOR=${ENABLE_REMOTE_INSPECTOR}
+            -DDEVELOPER_MODE=${DEVELOPER_MODE}
+
+            ${WEBKIT_CLANG_ARGS}
+
             BUILD_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --target JavaScriptCore --config ${CMAKE_BUILD_TYPE} --parallel
             INSTALL_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --target install --config ${CMAKE_BUILD_TYPE}
             UPDATE_COMMAND ""
@@ -110,10 +225,7 @@ function(setup_webkit_jsc)
         )
     else()
         target_link_libraries(JavaScriptCore_Dependencies INTERFACE
-                ${ICU_LIBRARIES} ${GLIB2_LIBRARIES} pthread dl m atomic
-        )
-        target_include_directories(JavaScriptCore_Dependencies INTERFACE
-                ${ICU_INCLUDE_DIRS} ${GLIB2_INCLUDE_DIRS}
+                pthread dl m atomic
         )
     endif()
 
@@ -122,7 +234,6 @@ function(setup_webkit_jsc)
             JavaScriptCore
             JavaScriptCore_Dependencies
     )
-    add_library(JavaScriptCore::Core ALIAS JavaScriptCore_Complete)
 
     message(STATUS "JSC Include directory: ${JSC_INCLUDE_DIR}")
 endfunction()
