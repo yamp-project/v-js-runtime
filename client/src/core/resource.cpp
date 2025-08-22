212c4f61b17c38b40c5a7c3eb01bba55eb4a8d7c
ByAlexius <personal.git@my-server.at>
2025-08-22 18:01:21 +0200

    feat: replace v8 with jscore in resource class

--- client/src/core/resource.cpp
+++ client/src/core/resource.cpp
@@ -2,64 +2,23 @@
 
 #include <filesystem>
 
+#include "JavaScriptCore/JavaScript.h"
+
 #include "runtime.h"
-#include "../template/global_template.h"
 #include "util/utils.h"
 
 namespace js {
-    Resource::Resource(ILookupTable* lookupTable, IResource* resource, v8::Isolate* isolate) : m_Resource(resource), m_Logger(Logger(lookupTable, std::format("resource {}", resource->name))), m_Isolate(isolate)
-    {
-    }
-
     void Resource::OnStart()
     {
         const std::filesystem::path resourcePath = m_Resource->path;
         const std::filesystem::path mainFilePath = m_Resource->mainFile;
-
-        v8::Isolate::Scope scope(m_Isolate);
-        v8::HandleScope handleScope(m_Isolate);
-
-        templates::GlobalTemplate global (m_Isolate, &m_Logger, this);
-
-        const v8::Local<v8::Context> context = v8::Context::New(m_Isolate, nullptr, global.GetGlobalTemplate());
-
-        context->SetAlignedPointerInEmbedderData(1, this);
-
-        v8::Context::Scope contextScope(context);
-
-        v8::Local<v8::String> jsSource = utils::StringToV8(m_Isolate, utils::ReadFile(m_Resource->mainFile));
-
-        v8::ScriptCompiler::Source scriptSource(jsSource);
-        v8::Local<v8::Module> scriptModule;
-        if (!v8::ScriptCompiler::CompileModule(m_Isolate, &scriptSource).ToLocal(&scriptModule)) {
-            m_Logger.Warn("Failed to compile script");
-            this->OnStop();
-            return;
-        }
-
-        const v8::Maybe<bool> result = scriptModule->InstantiateModule(context, ResolveCallback, nullptr);
-        if (result.IsNothing()) {
-            m_Logger.Warn("Failed to instantiate module");
-            this->OnStop();
-            return;
-        }
-
-        // This runs the javascript code
-        v8::Local<v8::Value> moduleResult;
-        if (!scriptModule->Evaluate(context).ToLocal(&moduleResult)) {
-            m_Logger.Warn("Failed to evaluate script");
-            this->OnStop();
-            return;
-        }
 
         m_Logger.Info("Running resource %s with main file %s and resource path %s!", m_Resource->name, mainFilePath.c_str(), resourcePath.c_str());
     }
 
     void Resource::OnStop()
     {
-        if (m_Isolate) {
-            m_Isolate->TerminateExecution();
-        }
+
     }
 
     void Resource::OnTick()
@@ -69,84 +28,43 @@ namespace js {
 
     void Resource::OnEvent(const CoreEventType type, const CAnyArray* args)
     {
-        const std::vector<v8::Local<v8::Function>> functions = m_CoreEventCallbacks[type];
-
-        if (functions.empty()) {
-            return;
-        }
-
-        v8::Local<v8::Value> funcArgs[args->size];
-
-        for (int i = 0; args->size <= i; i++) {
-            funcArgs[i] = (utils::ToV8Value(m_Isolate, *args->buffer[i]));
-        }
-
-        for (auto function : functions) {
-            if (function.IsEmpty()) {
-                continue;
-            }
-
-            function->Call(m_Isolate, m_Isolate->GetCurrentContext(), v8::Undefined(m_Isolate), args->size, funcArgs);
-        }
+        handleEvent(m_CoreEventCallbacks[type], args);
     }
 
     void Resource::OnEvent(const char* name, const CAnyArray* args)
     {
-        const std::vector<v8::Local<v8::Function>> functions = m_EventCallbacks[name];
+        handleEvent(m_EventCallbacks[name], args);
+    }
 
+    void Resource::handleEvent(const std::vector<JSObjectRef>& functions, const CAnyArray* args) {
         if (functions.empty()) {
             return;
         }
 
-        v8::Local<v8::Value> funcArgs[args->size];
+        std::vector<JSValueRef> funcArgs(args->size);
 
         for (int i = 0; args->size <= i; i++) {
-            funcArgs[i] = (utils::ToV8Value(m_Isolate, *args->buffer[i]));
+            funcArgs[i] = utils::ToJSValue(m_ContextWrapper->get(), *args->buffer[i]);
         }
 
         for (auto function : functions) {
-            if (function.IsEmpty()) {
+            if (!function) {
                 continue;
             }
 
-            function->Call(m_Isolate, m_Isolate->GetCurrentContext(), v8::Undefined(m_Isolate), args->size, funcArgs);
+            JSValueRef exception = nullptr;
+            JSValueRef result = JSObjectCallAsFunction(
+                m_ContextWrapper->get(),
+                function,
+                nullptr,
+                args->size,
+                funcArgs.data(),
+                &exception
+            );
+
+            if (exception) {
+                m_Logger.Error("An error (%s) occurred while running %s", exception, function);
+            }
         }
-    }
-
-    v8::MaybeLocal<v8::Module> Resource::ResolveCallback(const v8::Local<v8::Context> context,
-        const v8::Local<v8::String> specifier, [[maybe_unused]] const v8::Local<v8::FixedArray> import_attributes, [[maybe_unused]] const v8::Local<v8::Module> referrer) {
-
-        v8::Isolate* isolate = context->GetIsolate();
-
-        auto* selfRef = static_cast<Resource*>(context->GetAlignedPointerFromEmbedderData(1));
-
-        const std::string resourcePath = selfRef->GetResourcePath();
-
-        std::string specifierStr (*v8::String::Utf8Value(isolate, specifier));
-
-        const v8::MaybeLocal<v8::Module> processedModule = selfRef->GetProcessedModule(specifierStr);
-
-        if (!processedModule.IsEmpty()) {
-            return processedModule;
-        }
-
-        if (specifierStr.length() > 1 && specifierStr[0] == '.' && specifierStr[1] == '/') {
-            specifierStr = specifierStr.substr(1);
-        }
-
-        const std::string src = utils::ReadFile(resourcePath  + specifierStr);
-        if (src.empty()) {
-            return {};
-        }
-
-        v8::ScriptCompiler::Source scriptSource(utils::StringToV8(isolate, src));
-        v8::Local<v8::Module> scriptModule;
-        if (!v8::ScriptCompiler::CompileModule(isolate, &scriptSource).ToLocal(&scriptModule)) {
-            return {};
-        }
-
-        selfRef->AddProcessedModule(specifierStr, scriptModule);
-
-        return {scriptModule};
     }
 } // js
--- client/src/core/resource.h
+++ client/src/core/resource.h
@@ -6,7 +6,10 @@
 #include "util/logger.h"
 
 #include <yamp-sdk/sdk.h>
-#include <v8.h>
+#include <JavaScriptCore/JavaScript.h>
+
+#include "runtime.h"
+#include "../wrapper/JSContextWrapper.h"
 
 namespace js {
     class Resource {
@@ -17,7 +20,9 @@ namespace js {
         void OnEvent(CoreEventType type, const CAnyArray* args);
         void OnEvent(const char* name, const CAnyArray* args);
 
-        Resource(ILookupTable* lookupTable, IResource* resource, v8::Isolate* isolate);
+        Resource(ILookupTable* lookupTable, IResource* resource)
+                    : m_Resource(resource), m_Logger(Logger(lookupTable, std::format("resource {}", resource->name))), m_ContextWrapper(std::make_unique<wrapper::JSContextWrapper>(GetGlobalTemplate())) {}
+
         ~Resource() = default;
 
         [[nodiscard]]
@@ -35,42 +40,25 @@ namespace js {
             return m_Resource->name;
         }
 
-        v8::MaybeLocal<v8::Module> GetProcessedModule(const std::string& specifier) {
-            if (m_ProcessedModules.contains(specifier)) {
-                return m_ProcessedModules[specifier].Get(m_Isolate);
-            }
-
-            return {};
-        }
-
-        void AddProcessedModule(const std::string &specifier, v8::Local<v8::Module> module) {
-            m_ProcessedModules[specifier].Reset(m_Isolate, module);
-        }
-
-        void AddCoreEventCallback(const CoreEventType event, const v8::Local<v8::Function> callback) {
+        void AddCoreEventCallback(const CoreEventType event, const JSObjectRef callback) {
             m_CoreEventCallbacks[event].push_back(callback);
         }
 
-        void AddEventCallback(const std::string& event, const v8::Local<v8::Function> callback) {
+        void AddEventCallback(const std::string& event, const JSObjectRef callback) {
             m_EventCallbacks[event].push_back(callback);
         }
 
     private:
-        static v8::MaybeLocal<v8::Module> ResolveCallback(v8::Local<v8::Context> context,
-                                                v8::Local<v8::String> specifier,
-                                                v8::Local<v8::FixedArray> import_attributes,
-                                                v8::Local<v8::Module> referrer);
+        void handleEvent(const std::vector<JSObjectRef>& functions, const CAnyArray* args);
 
-        std::map<std::string, v8::Global<v8::Module>> m_ProcessedModules;
-
+    private:
         IResource* m_Resource;
         Logger m_Logger;
 
-        std::map<CoreEventType, std::vector<v8::Local<v8::Function>>> m_CoreEventCallbacks;
-        std::map<std::string, std::vector<v8::Local<v8::Function>>> m_EventCallbacks;
+        std::map<CoreEventType, std::vector<JSObjectRef>> m_CoreEventCallbacks;
+        std::map<std::string, std::vector<JSObjectRef>> m_EventCallbacks;
 
-        v8::Isolate* m_Isolate;
-        v8::Local<v8::ObjectTemplate> m_GlobalTemplate;
+        std::unique_ptr<wrapper::JSContextWrapper> m_ContextWrapper;
     };
 } // js
 
